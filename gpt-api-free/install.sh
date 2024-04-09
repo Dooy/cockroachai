e8c2c88272c3136cc83291ecb090c8dad4d37d20
Dooy <dooy520@qq.com>
2024-04-09 13:21:32 +0800

    free

---
+++ gpt-api-free/install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+docker compose pull
+docker compose up -d --remove-orphans
+#docker compose restart
+
+#老板docker 下面的
+# docker-compose pull
+# docker-compose up -d --remove-orphans
