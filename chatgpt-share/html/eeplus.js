b7f0537ae437d10eea09ca6f540ec2efa7c51119
Dooy <dooy520@qq.com>
2024-04-12 10:18:54 +0800

    free

---
+++ chatgpt-share/html/eeplus.js
@@ -0,0 +1,32 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?2d987a7a5924beded73be64dd7fe4b9b";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+
+$(function () {
+    var $div = $("<div></div>");
+    $div.css({
+      "border-top-left-radius": "34px",
+      "border-bottom-left-radius": "34px",
+      background: "linear-gradient(140.91deg, #FF87B7 12.61%, #EC4C8C 76.89%)",
+      height: "34px",
+      width: "45px",
+      margin: "1px",
+      display: "flex",
+      "align-items": "center",
+      position: "fixed",
+      right: "0px",
+      top: `50px`,
+      cursor: "pointer",
+    });
+    $div.html(
+      "<span style='color:white;font-size:15px;margin-left:10px'>换通道</span>"
+    );
+    $("body").append($div);
+    $div.click(function () {
+      window.location.href = "/list";
+    });
+  });
