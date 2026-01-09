ce07fc3fcb6a32ef2105fb3e90a995265a04d99e
Dooy <dooy520@qq.com>
2026-01-09 12:29:16 +0800

    good

--- mitmproxy/start.sh
+++ mitmproxy/start.sh
@@ -46,6 +46,7 @@ if [ -n "$PID" ]; then
     echo "✅ 启动成功！"
     echo "进程 PID: $PID"
     echo "代理地址: http://$USER_AUTH@$PUBLIC_IP:$PORT"
+    echo "代理地址: socks5://$USER_AUTH@$PUBLIC_IP:8081"
     echo "内网代理地址: http://$USER_AUTH@127.0.0.1:$PORT"
 else
     echo "❌ 启动失败，请检查端口 $PORT 是否被占用或 Python 环境是否正常。"
